aaf836cebcbecfae06f7942f380c09241df9619c
yx02 <yangxin02@weidian.com>
2021-12-06 15:18:53 +0800

    format

--- lib/ui/wd_ext_api.dart
+++ lib/ui/wd_ext_api.dart
@@ -6,26 +6,23 @@
 // @dart = 2.12
 part of dart.ui;
 
-
 class WDExtApi {
-  
   // This class is only a namespace, and should not be instantiated or
   // extended directly.
   factory WDExtApi._() => throw UnsupportedError('Namespace');
 
-  /// 
+  /// 查询app的dart层相关内存数据.
+  /// 暂时只支持iOS
   static String queryMainIsolateMemory() {
     return _queryMainIsolateMemory();
   }
 
-  /// 
+  /// 查询app的dart层相关内存数据，包括系统的.
+  /// 暂时只支持iOS
   static String queryAllIsolateMemory() {
     return _queryAllIsolateMemory();
   }
 
-  //
   static String _queryMainIsolateMemory() native 'WDEXTAPI_SQueryMainIsolateMemory';
-
-  //
   static String _queryAllIsolateMemory() native 'WDEXTAPI_SQueryAllIsolateMemory';
 }
